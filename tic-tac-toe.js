3a90fc2cebec936240a0b3265225f7400aa334bb
JodyHarri101 <Jody.harrison32@gmail.com>
2020-10-08 23:43:32 -0500

    Exercise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -26,9 +26,28 @@ window.onload = function () {
       console.log(player1Array);
       console.log(player2Array);
     });
+
+    boardgame.item(i).onmouseenter = function () {
+      mouseEnter();
+    };
+    function mouseEnter() {
+      boardgame.item(i).classList.add("hover");
+    }
+    boardgame.item(i).onmouseleave = function () {
+      mouseLeave();
+    };
+    function mouseLeave() {
+      boardgame.item(i).classList.remove("hover");
+    }
   }
 
-  //let box = boardgame[i];
   var player1Array = new Array(5);
   var player2Array = new Array(4);
+
+  // document
+  // .getElementsById("hover")
+  // .addEventListener("mouseover", mouseOver);
+  // document
+  //   .getElementsByClassName("hover")
+  //  .addEventListener("mouseout", mouseOut);
 };

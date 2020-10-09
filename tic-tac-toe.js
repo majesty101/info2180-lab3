df25bf64dcb743a70c0dd2dc8ed6914a9ff6108a
JodyHarri101 <Jody.harrison32@gmail.com>
2020-10-08 21:42:52 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,4 +6,29 @@ window.onload = function () {
     brdgame.classList.add("square");
     console.log(brdgame.classList);
   }
+
+  var player1turn = 10;
+  for (let i = 0; i < boardgame.length; i++) {
+    boardgame.item(i).addEventListener("click", function () {
+      if (player1turn % 2 == 0) {
+        boardgame.item(i).innerHTML = "X";
+        boardgame.item(i).classList.add("X");
+        player1Array.push(1);
+      } else {
+        boardgame.item(i).innerHTML = "O";
+        boardgame.item(i).classList.add("O");
+        player2Array.push(1);
+      }
+      player1turn--;
+      //player2turn--;
+
+      console.log("you clicked box number" + i);
+      console.log(player1Array);
+      console.log(player2Array);
+    });
+  }
+
+  //let box = boardgame[i];
+  var player1Array = new Array(5);
+  var player2Array = new Array(4);
 };
